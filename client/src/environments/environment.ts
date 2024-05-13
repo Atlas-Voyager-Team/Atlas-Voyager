b5cd7ff93ad210f077d00ac8c28bf2b6827527ef
Daniel <daniel_aleksandrov@yahoo.com>
2024-05-13 15:00:37 -0400

    merge

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    serverUrl: 'http://localhost:3000/api',
+    serverUrl: 'http://ec2-3-99-142-253.ca-central-1.compute.amazonaws.com:3000/api',
     socketUrl: 'http://localhost:3000',
 };
